bbbf18819f9c1b79758096374da36dae7e6e3959
Viktoriia Hrabenko <vikagrabenko666@gmail.com>
2023-02-10 16:26:29 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 axios.default.baseURL = 'https://api.themoviedb.org/3';
 const API_KEY = '69096c6df3db45b0788fb44bda629200';
@@ -7,11 +7,11 @@ export const getTrendingApi = async () => {
   const response = await axios.get(
     `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
   );
-  
+
   return response.data.results;
 };
 
-export const getMovieSearch = async (query) => {
+export const getMovieSearch = async query => {
   const response = await axios.get(
     `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${API_KEY}`
   );
@@ -19,22 +19,23 @@ export const getMovieSearch = async (query) => {
   return response.data.results;
 };
 
-export const getMovieDetailsById = async (id) => {
+export const getMovieDetailsById = async id => {
   const response = await axios.get(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
   );
+
   return response.data;
 };
 
-export const getMovieCredits = async (id) => {
+export const getMovieCredits = async id => {
   const response = await axios.get(
     `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}`
   );
-  
+
   return response.data.cast;
 };
 
-export const getMovieReviews = async (id) => {
+export const getMovieReviews = async id => {
   const response = await axios.get(
     `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${API_KEY}`
   );
